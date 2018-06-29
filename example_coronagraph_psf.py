e2fd2c8d482f825eef9a06f68dfac5224e980b7b
Prashant Pathak <ppathak8@gmail.com>
2018-06-29 16:32:15 +0200

    Add files via upload

--- example_coronagraph_psf.py
+++ example_coronagraph_psf.py
@@ -17,6 +17,7 @@ from heeps import * # loads all HEEPS scripts required for simuation
 #  default simulation cofiguration defined in "simulation_config" can be overridden here
 wavelength = 3.80*10**-6
 charge = 2 # charge is modified here
+TILT = np.array(((0,0),(.1,.1)))#np.array([0.,0.])
 
 
 # =============================================================================
@@ -29,34 +30,16 @@ charge = 2 # charge is modified here
 # =============================================================================
 #                      Wavefront abberations
 # =============================================================================
-TILT = np.array(((0,0),(.1,.1)))#np.array([0.,0.])
 
-wavefront_abberations(wfo, npupil, atm_screen, NCPA,Island_Piston,TILT=TILT[1], Debug='False', 
-          Debug_print='False', prefix='test')  
+wavefront_abberations(wfo, npupil, atm_screen, NCPA,Island_Piston,TILT=TILT[0], Debug='False', Debug_print='False', prefix='test')  
 
 
 # =============================================================================
-#       Coronagraph selection -- Classical Vortex / RAVC / APP --
+#       Coronagraph selection -- Vortex Classical (VC) / RAVC / APP --
 # =============================================================================
 
-
-#   (1) __________  Apodization (for RAVC and APP)  __________________________
-
-
-RAVC = False
-
-apodization(wfo, r_obstr, npupil, RAVC=RAVC, phase_apodizer_file=phase_apodizer_file, amplitude_apodizer_file=amplitude_apodizer_file, apodizer_misalignment=apodizer_misalignment, Debug_print=Debug_print)
-
-#   (2)  __________  Coronagraph mask (for vortex or RAVC )  __________________
-
-
-if (Vortex == True):
-    vortex(wfo, charge, f_lens,diam, pixelsize, Debug_print = Debug_print)
-
-##  (3)  _____________       lyot stop ________________________________________
-
-lyotstop(wfo, diam, r_obstr, npupil, RAVC, LS, LS_parameters, spiders_angle, LS_phase_apodizer_file, 
-         LS_amplitude_apodizer_file, LS_misalignment, Debug_print, Debug)
+coronagraphs(wfo, r_obstr,npupil, phase_apodizer_file,amplitude_apodizer_file,apodizer_misalignment,charge,
+              f_lens,diam,LS_amplitude_apodizer_file,LS_misalignment,LS,LS_parameters,spiders_angle, LS_phase_apodizer_file, Debug_print,pixelsize, Debug,coronagraph_type= 'VC')
 
 
 # =============================================================================
--- example_multi_cube_abberations.py
+++ example_multi_cube_abberations.py
@@ -5,35 +5,37 @@
 # =============================================================================
 
 # Required libraries
-#import proper # library for propagation of wavefront
+import proper # library for propagation of wavefront
 import matplotlib.pyplot as plt # for plotting simulated PSFs
 import numpy as np
 from astropy.io import fits    # to read and write fits files
 
 from simulation_config import *	# Loads default configuration for simulation (later will be changed to dictinary)
 from heeps import * # loads all HEEPS scripts required for simuation
-
+from copy import deepcopy
 
 #  default simulation cofiguration defined in "simulation_config" can be overridden here
 wavelength = 3.80*10**-6
 charge = 2 # charge is modified here
-n = gridsize
+#n = gridsize
 
 # =============================================================================
 #                         ELT Pupil Plane
 # =============================================================================
 
-
+(npupil, wfo1) = pupil(diam, gridsize, spiders_width, spiders_angle, pixelsize,  r_obstr, wavelength,
+          pupil_file=pupil_file, missing_segments_number=0, Debug=Debug, Debug_print=Debug_print, prefix=prefix) 
+  
 # =============================================================================
 #                      Wavefront abberations
 # =============================================================================
 
-(npupil, wfo1) = pupil(diam, gridsize, spiders_width, spiders_angle, pixelsize,  r_obstr, wavelength,
-        pupil_file=pupil_file, missing_segments_number=0, Debug=Debug, Debug_print=Debug_print, prefix=prefix) 
+#proper.verbose = False
+#QUIT = True
+proper.print_it = True
 
 if (atm_screen.ndim == 3) or (TILT.ndim == 2) or (LS_misalignment.ndim == 2) or (apodizer_misalignment.ndim == 2) or (Island_Piston.ndim == 2) or (NCPA.ndim == 3):
-    print('Cube')
-    
+   
     if (atm_screen.ndim == 3):
         length_cube = atm_screen.shape[0]
     if (TILT.ndim == 2):
@@ -47,12 +49,12 @@ if (atm_screen.ndim == 3) or (TILT.ndim == 2) or (LS_misalignment.ndim == 2) or 
     if (NCPA.ndim == 3):
         length_cube = NCPA.shape[0]
     
-    wfo = deepcopy(wfo1)
+#    wfo = deepcopy(wfo1)
     psf_Coro = np.zeros((length_cube,nd,nd))
 
-    
+  
     for iter in range(0, length_cube):
-        print('iter: ', iter)
+        wfo = deepcopy(wfo1)
 
         if ((isinstance(atm_screen, (list, tuple, np.ndarray)) == True)):
             if (atm_screen.ndim == 3):
@@ -61,7 +63,6 @@ if (atm_screen.ndim == 3) or (TILT.ndim == 2) or (LS_misalignment.ndim == 2) or 
                 atm_screen_iter = atm_screen
         if (TILT.ndim == 2):
             TILT_iter = TILT[iter,:]
-            print('TILT: ', TILT_iter)
         else:
             TILT_iter = TILT
         if (LS_misalignment.ndim == 2):
@@ -75,38 +76,22 @@ if (atm_screen.ndim == 3) or (TILT.ndim == 2) or (LS_misalignment.ndim == 2) or 
         if (Island_Piston.ndim == 2):
             Island_Piston_iter = Island_Piston[iter,:]
         else:
-            Island_Piston_iter = Island_Piston
+            Island_Piston_iter = Island_Piston          
         if (isinstance(NCPA, (list, tuple, np.ndarray)) == True):
             if (NCPA.ndim == 3):
                 NCPA_iter = NCPA[iter,:,:]
             else:
                 NCPA_iter = NCPA
-             
-
+                
+        
         wavefront_abberations(wfo, npupil, atm_screen_iter, NCPA,Island_Piston,TILT=TILT_iter, Debug='False',Debug_print='False', prefix='test')  
         # =============================================================================
         #       Coronagraph selection -- Classical Vortex / RAVC / APP --
         # =============================================================================
         
-        
-        #   (1) __________  Apodization (for RAVC and APP)  __________________________
-        
-        
-        RAVC = False
-        
-        apodization(wfo, r_obstr, npupil, RAVC=RAVC, phase_apodizer_file=phase_apodizer_file, amplitude_apodizer_file=amplitude_apodizer_file, apodizer_misalignment=apodizer_misalignment, Debug_print=Debug_print)
-        
-        #   (2)  __________  Coronagraph mask (for vortex or RAVC )  __________________
-        
-        
-        if (Vortex == True):
-            vortex(wfo, charge, f_lens,diam, pixelsize, Debug_print = Debug_print)
-        
-        ##  (3)  _____________       lyot stop ________________________________________
-        
-        lyotstop(wfo, diam, r_obstr, npupil, RAVC, LS, LS_parameters, spiders_angle, LS_phase_apodizer_file, 
-                 LS_amplitude_apodizer_file, LS_misalignment, Debug_print, Debug)
-        
+        coronagraphs(wfo, r_obstr,npupil, phase_apodizer_file,amplitude_apodizer_file,apodizer_misalignment,charge,
+              f_lens,diam,LS_amplitude_apodizer_file,LS_misalignment,LS,LS_parameters,spiders_angle, LS_phase_apodizer_file, Debug_print,pixelsize, Debug,coronagraph_type= 'RAVC')
+
         
         # =============================================================================
         #       Detector plane
