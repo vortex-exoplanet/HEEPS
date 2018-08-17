05c2a1688cc85c0ab40a2340bd052644171c8898
Prashant Pathak <ppathak8@gmail.com>
2018-08-17 14:51:59 +0200

    Add files via upload

--- example_coronagraph_psf.py
+++ example_coronagraph_psf.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # =============================================================================
-#       Example file for creating a coronagraphic/non-coronagraphic PSF
+#       Example file for creating a coronagraphic/non-coronagraphic METIS PSF
 # =============================================================================
 
 # Required libraries
@@ -11,36 +11,33 @@ from astropy.io import fits
 from read_config import conf
 from heeps import metis_hci, download_from_gdrive
 
-
-conf
-
 """
-Default simulation cofiguration defined in "simulation_config.md" can be 
+Default simulation cofiguration defined in "read_config.py" can be 
 overridden here by updating the dictionary
 """
-
+conf
 conf['WAVELENGTH'] = 3.80*10**-6 
 conf['CHARGE'] = 2 # charge is modified here
 
-atm_screen = fits.getdata(conf['INPUT_DIR'] + conf['ATM_SCREEN_2D'])
 
-gdrive_id = '1AUtELRfn_xjnbsMM_SJG6W0c26zyzqNH'    # [Required] google drive id linked to the fits file 
-atm_screen_cube  = conf['ATM_SCREEN_CUBE']
-download_from_gdrive(gdrive_id, conf['INPUT_DIR'], atm_screen_cube) 
+#atm_screen = 0 # no atmospheric phase screen
+
+# 2-D pahse screen
+#atm_screen = fits.getdata(conf['INPUT_DIR'] + conf['ATM_SCREEN_2D']) 
+
+# getting multi-cube phase screen
+download_from_gdrive(conf['GDRIVE_ID'], conf['INPUT_DIR'], conf['ATM_SCREEN_CUBE']) 
 atm_screen = fits.getdata(conf['INPUT_DIR']+ conf['ATM_SCREEN_CUBE'])[0:10] 
 
-TILT = np.array(conf['TILT_2D'])
-#TILT = np.random.randn(10,2)
+TILT = np.array(conf['TILT_2D']) 
+#TILT = np.random.randn(conf['TILT_CUBE'],2)
 
 
-mode = 'VC'
-conf['MODE'] = mode
-
-psf = metis_hci(mode , conf, atm_screen, TILT)            
+psf = metis_hci(conf['MODE'], conf, atm_screen, TILT)            
 
 
 plt.figure()
-plt.imshow(psf**0.25)
+plt.imshow(psf**0.5)
 plt.colorbar()
 
 plt.show()
