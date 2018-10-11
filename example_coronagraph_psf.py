db6bc220218b547480ac9d6b3c9f599f1d5d634d
Christian <christian.delacroix@gmail.com>
2018-10-11 17:20:15 +0200

    update to example file, addressing #7

--- example_coronagraph_psf.py
+++ example_coronagraph_psf.py
@@ -4,42 +4,49 @@
 #       Example file for creating a coronagraphic/non-coronagraphic METIS PSF
 # =============================================================================
 
-# Required libraries
+""" Required libraries """
 import matplotlib.pyplot as plt # for plotting simulated PSFs
 import numpy as np
 from astropy.io import fits 
+from heeps.config import conf, pre_sim, download_from_gdrive
+from heeps.pupil import pupil
+from heeps.abberations import wavefront_abberations
+from heeps.coronagraphs import apodization, vortex, lyotstop
+from heeps.detector import detector
 
-from heeps.config import download_from_gdrive
-from heeps import metis_hci
-from heeps.config import conf, pre_sim
-
+""" Default simulation configuration defined in "read_config.py" can be 
+overridden here by updating the dictionary """
+conf['WAVELENGTH'] = 3.80*10**-6 
+conf['STATIC_NCPA'] = False
+conf['TILT_2D'] = np.zeros(2) #np.random.randn(conf['TILT_CUBE'],2)
+conf['ATM_SCREEN'] = 0
+conf['RAVC'] = False
+conf['PHASE_APODIZER_FILE'] = 0
 pre_sim(conf) 
 
-"""
-Default simulation cofiguration defined in "read_config.py" can be 
-overridden here by updating the dictionary
-"""
-conf['WAVELENGTH'] = 3.80*10**-6 
-conf['CHARGE'] = 2 # charge is modified here
-conf['MODE'] = 'VC'
-conf['STATIC_NCPA'] = False
+""" Pupil """
+wfo = pupil(conf) 
 
-# 2-D pahse screen
-atm_screen = 0 
+""" Wavefront errors, getting multi-cube phase screen from Google Drive"""
+get_cube = True
+if get_cube is True:
+	download_from_gdrive(conf['GDRIVE_ID'], conf['INPUT_DIR'], conf['ATM_SCREEN_CUBE']) 
+	conf['ATM_SCREEN'] = fits.getdata(conf['INPUT_DIR']+ conf['ATM_SCREEN_CUBE'])[0] 
+wfo = wavefront_abberations(wfo, conf, conf['ATM_SCREEN'], conf['TILT_2D'])
 
-# getting multi-cube phase screen
-download_from_gdrive(conf['GDRIVE_ID'], conf['INPUT_DIR'], conf['ATM_SCREEN_CUBE']) 
-atm_screen = fits.getdata(conf['INPUT_DIR']+ conf['ATM_SCREEN_CUBE'])[0] 
+""" Coronagraph """
+RAVC = False
+wfo = apodization(wfo, conf, conf['RAVC'])
+wfo = vortex(wfo, conf)
+wfo = lyotstop(wfo, conf, conf['RAVC'])
 
-TILT = np.array(conf['TILT_2D']) 
-#TILT = np.random.randn(conf['TILT_CUBE'],2)
+""" Science image """
+psf = detector(wfo, conf)
+fits.writeto(conf['OUT_DIR'] + conf['PREFIX'] + '_PSF_'+ conf['MODE'] +'.fits', psf, overwrite=True)        
 
-psf = metis_hci(conf, atm_screen, TILT)
-
-
+""" Figure """
 plt.figure()
 plt.imshow(psf**0.05)
 plt.colorbar()
-
-plt.show()
+plt.show(block=False)
 
