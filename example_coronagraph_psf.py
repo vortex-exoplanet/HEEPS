cb906bfaf5ca3a63a4820198f4303933726b5c39
Christian <christian.delacroix@gmail.com>
2018-10-16 14:19:18 +0200

    adding mode selection; using os.path.join

--- example_coronagraph_psf.py
+++ example_coronagraph_psf.py
@@ -13,6 +13,7 @@ from heeps.pupil import pupil
 from heeps.abberations import wavefront_abberations
 from heeps.coronagraphs import apodization, vortex, lyotstop
 from heeps.detector import detector
+import os.path
 
 """ Default simulation configuration defined in "read_config.py" can be 
 overridden here by updating the dictionary """
@@ -20,33 +21,43 @@ conf['WAVELENGTH'] = 3.80*10**-6
 conf['STATIC_NCPA'] = False
 conf['TILT_2D'] = np.zeros(2) #np.random.randn(conf['TILT_CUBE'],2)
 conf['ATM_SCREEN'] = 0
-conf['RAVC'] = False
 conf['PHASE_APODIZER_FILE'] = 0
+conf['MODE'] = 'ELT'
 pre_sim(conf) 
 
 """ Pupil """
+conf['PUPIL_FILE'] = 'ELT_2048_37m_11m_5mas_nospiders_cut.fits'
 wfo = pupil(conf) 
 
 """ Wavefront errors, getting multi-cube phase screen from Google Drive"""
 get_cube = True
 if get_cube is True:
-	download_from_gdrive(conf['GDRIVE_ID'], conf['INPUT_DIR'], conf['ATM_SCREEN_CUBE']) 
-	conf['ATM_SCREEN'] = fits.getdata(conf['INPUT_DIR']+ conf['ATM_SCREEN_CUBE'])[0] 
+    download_from_gdrive(conf['GDRIVE_ID'], conf['INPUT_DIR'], conf['ATM_SCREEN_CUBE']) 
+    conf['ATM_SCREEN'] = fits.getdata(conf['INPUT_DIR']+ conf['ATM_SCREEN_CUBE'])[0] 
 wfo = wavefront_abberations(wfo, conf, conf['ATM_SCREEN'], conf['TILT_2D'])
 
 """ Coronagraph """
-RAVC = False
-wfo = apodization(wfo, conf, conf['RAVC'])
-wfo = vortex(wfo, conf)
-wfo = lyotstop(wfo, conf, conf['RAVC'])
+if conf['MODE'] == 'ELT': # no Lyot stop (1, -0.3, 0)
+    conf['LS_PARA'] = [1., -0.3, 0.]
+    _, LS_pupil = lyotstop(wfo, conf, RAVC=False)
+elif conf['MODE'] == 'OFFAXIS': # only lyot stop
+    lyotstop(wfo, conf, RAVC=False)
+elif conf['MODE'] == 'VC':
+    vortex(wfo, conf)
+    lyotstop(wfo, conf, RAVC=False)
+elif conf['MODE'] == 'RAVC':
+    apodization(wfo, conf, RAVC=True)
+    vortex(wfo, conf)
+    lyotstop(wfo, conf, RAVC=True)
+elif conf['MODE'] == 'MASK': # only ring apodizer and lyot stop
+    apodization(wfo, conf, RAVC=True)
+    lyotstop(wfo, conf, RAVC=True)
+elif conf['MODE'] == 'APP': 
+    conf['PHASE_APODIZER_FILE'] = 'app_phase_cut.fits'
+    lyotstop(wfo, conf, RAVC=False)
 
 """ Science image """
+filename_PSF = conf['PREFIX'] + '_PSF_' + conf['MODE']
+filename_LS = conf['PREFIX'] + '_LS_' + conf['MODE']
 psf = detector(wfo, conf)
-fits.writeto(conf['OUT_DIR'] + conf['PREFIX'] + '_PSF_'+ conf['MODE'] +'.fits', psf, overwrite=True)        
-
-""" Figure """
-plt.figure()
-plt.imshow(psf**0.05)
-plt.colorbar()
-plt.show(block=False)
-
+fits.writeto(os.path.join(conf['OUT_DIR'], filename_PSF) + '.fits', psf, overwrite=True)
