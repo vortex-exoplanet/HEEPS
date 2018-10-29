32038761a8a64205b9ddd5802ee04c3f6969aeaa
Christian <christian.delacroix@gmail.com>
2018-10-29 16:15:29 +0100

    atmosphere.py cleanup
    TODO: allow cube of phase screens as input

--- heeps/abberations/atmosphere.py
+++ heeps/abberations/atmosphere.py
@@ -1,34 +1,39 @@
 import numpy as np
 import cv2
 import proper
-import math
-from astropy.io import fits
+import astropy.units as u
 
-def atmosphere(wf, npupil, atm_screen, Debug_print, Debug):
+def atmosphere(wfo, npupil, phase_screen):
+    """ Compute the atmosphere phase screens as exp(iϕ(2π/λ)) and apply them to 
+    the wavefront. The phase screen can be a single 2D array (nx, ny), or a cube 
+    of N phase screens (N, nx, ny).
+    """
     
-    n = int(proper.prop_get_gridsize(wf))
-    lamda=proper.prop_get_wavelength(wf) #save the wavelength value [m] into lamda
-
-    screen = atm_screen # read the phase screen
-    if (Debug_print == True):
-        print ("screen", screen.shape)
-    screen_pixels = (screen.shape)[0] # size of the phase screen
-    scaling_factor_screen = float(npupil)/float(screen_pixels) # scaling factor the phase screen to the simulation pupil size
-    if (Debug_print == True):
-        print ("scaling_factor_screen: ", scaling_factor_screen)
-    screen_scale = cv2.resize(screen.astype(np.float32), (0,0), fx=scaling_factor_screen, fy=scaling_factor_screen, interpolation=cv2.INTER_LINEAR) # scale the the phase screen to the simulation pupil size
-    if (Debug_print == True):
-        print ("screen_resample", screen_scale.shape)
-    screen_large = np.zeros((n,n)) # define an array of n-0s, where to insert the screen
-    if (Debug_print == True):
-        print("n: ", n)
-        print("npupil: ", npupil)
-    screen_large[int(n/2)+1-int(npupil/2)-1:int(n/2)+1+int(npupil/2),int(n/2)+1-int(npupil/2)-1:int(n/2)+1+int(npupil/2)] =screen_scale # insert the scaled screen into the 0s grid
-
-
-    lambda2=lamda/(1e-6) # need to use lambda in microns
-    screen_atm = np.exp(1j*screen_large/lambda2*2*math.pi)
-    proper.prop_multiply(wf, screen_atm) # multiply the atm screen to teh wavefront
-
-
-    return wf
+    # pupil size must be odd (PROPER sets the center up-right next to the grid center)
+    npupil = int(npupil + 1) if npupil % 2 == 0 else int(npupil)
+    # reshape the phase screens to a cube [N × nx × ny] where N = 1 by default
+    phase_screen = np.array(phase_screen, ndmin=3, dtype='float32')
+    
+    # get the number of screens
+    ncube = phase_screen.shape[0]
+    # get the numbers of pixels in x and y
+    nx, ny = phase_screen.shape[1:3]
+    # get the scaling factors from phase screens to simulation pupil size
+    fx, fy = np.float64(npupil)/(nx, ny)
+    # create a function for rescaling the phase screens
+    rescale = lambda p: cv2.resize(p, (0,0), fx=fx, fy=fy, interpolation=cv2.INTER_LINEAR)
+    # now, rescale the phase screens, and pad with zeros to match PROPER gridsize
+    r = int((proper.prop_get_gridsize(wfo) - npupil)/2)
+    phi = np.array([np.pad(rescale(phase_screen[x,:,:]), [(r+1,r),(r+1,r)], \
+            mode='constant') for x in range(ncube)])
+    
+    # get the wavelength in microns
+    lam = (proper.prop_get_wavelength(wfo)*u.m).to('um').value
+    # get the wavenumber (spatial angular frequency)
+    k = 2*np.pi/lam
+    # get the complex atmosphere phase screens (TODO: allow cubes)
+    atm_screen = np.exp(1j*k*phi[0])
+    # multiply the atmosphere phase screens to the wavefront
+    proper.prop_multiply(wfo, atm_screen)
+    
+    return wfo
