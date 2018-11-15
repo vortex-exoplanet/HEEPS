7dcc1d943b18f58c2a8ec591f53f7e3029988418
Prashant Pathak <ppathak8@gmail.com>
2018-11-15 15:02:09 +0100

    "metis_modes" cleaned up a bit

--- heeps/coronagraphs/metis_modes.py
+++ heeps/coronagraphs/metis_modes.py
@@ -4,39 +4,39 @@ from .lyotstop import lyotstop
 from .lyot import lyot
 
 def metis_modes(wfo, conf):
+    """
+    Various METIS modes Coronagraphic and non-coronagraphics:
+        1. Vortex coronagraph (VC), to use put conf['MODE'] = 'VC'
+        2. Ring apodized vortex coronagraph (RAVC), to use put conf['MODE'] = 'RAVC'
+        3. Apodizing phase plate (APP), to use put conf['MODE'] = 'APP'
+        4. No coronagraph, just Lyot-stop, to use put conf['MODE'] = 'OFFAXIS'    
+        5. No coronagraph, but Ring apodizer and LS present, to use put conf['MODE'] = 'MASK'
+        6. If conf['MODE'] = ELT, an ELT psf is generated    
+        7. Classical Lyot coronagraph, to use put conf['MODE'] = 'CL' 
+    """
     mode = conf['MODE']
-    if mode == 'APP': 
-        RAVC = False    
-        lyotstop(wfo, conf, RAVC)
-    
     conf['PHASE_APODIZER_FILE'] = 0
+
+    if mode == 'APP': 
+        conf['PHASE_APODIZER_FILE'] = 'app_phase_cut.fits' 
+        lyotstop(wfo, conf, RAVC=False)   
     if mode == 'RAVC':    
-        RAVC = True
         apodization(wfo, conf, RAVC=True)
         vortex(wfo, conf)
-        lyotstop(wfo, conf, RAVC)
-    elif conf['MODE'] == 'CL': # classical lyot
+        lyotstop(wfo, conf, RAVC=True)
+    elif mode == 'CL': # classical lyot
         lyot(wfo, conf)
-        _, LS_pupil = lyotstop(wfo, conf, RAVC=False)
+        lyotstop(wfo, conf, RAVC=False)
     elif mode == 'VC':
-        RAVC = False
         vortex(wfo, conf)
-        lyotstop(wfo, conf, RAVC)
+        lyotstop(wfo, conf, RAVC=False)
     elif mode == 'OFFAXIS':
         print('No Coronagraph')    
-        RAVC = False
-        lyotstop(wfo, conf, RAVC)
+        lyotstop(wfo, conf, RAVC=False)
     elif mode == 'MASK':
         print('Ring apodizer and LS present')    
-        RAVC = True
         apodization(wfo, conf, RAVC=True)
-        lyotstop(wfo, conf, RAVC)
-    else:
-        print('ELT PSF')    
+        lyotstop(wfo, conf, RAVC=True)
+    else: 
+        print('ELT PSF')
     return wfo
-
-
-
-
-
-
