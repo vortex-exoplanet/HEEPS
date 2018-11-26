a9d99162b8ef957fee1dc9eafd688b0634fa3746
Christian <christian.delacroix@gmail.com>
2018-11-26 15:05:44 +0100

    download_from_gdrive cleanup

--- heeps/config/download_from_gdrive.py
+++ heeps/config/download_from_gdrive.py
@@ -3,16 +3,17 @@ import os.path
 
 '''Downloads files from google drive'''
 
-def download_from_gdrive(id, destination, filename):
+def download_from_gdrive(id, destination, filename, 
+        url='https://docs.google.com/uc?export=download'):
+    # path to file
     my_file = str(os.path.join(destination, filename))
     if not os.path.isfile(my_file): 
         print('Downloading "%s" from google drive' %filename)
-        URL = "https://docs.google.com/uc?export=download"
         session = requests.Session()
-        response = session.get(URL, params={'id':id}, stream=True)
+        response = session.get(url, params={'id':id}, stream=True)
         token = get_confirm_token(response)
         if token:
-            response = session.get(URL, params={'id':id, 'confirm':token}, stream=True)
+            response = session.get(url, params={'id':id, 'confirm':token}, stream=True)
         save_response_content(response, my_file)
 
 def get_confirm_token(response):
@@ -22,7 +23,6 @@ def get_confirm_token(response):
     return None
 
 def save_response_content(response, my_file, chunk_size=32768):
-    
     with open(my_file, "wb") as f:
         for chunk in response.iter_content(chunk_size=chunk_size):
             if chunk: # filter out keep-alive new chunks
