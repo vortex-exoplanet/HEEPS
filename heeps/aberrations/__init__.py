9002622eb090413bb20ab407897d342958dd7b91
Prashant Pathak <ppathak8@gmail.com>
2019-04-15 15:39:35 +0200

    adding quasi-static ncpa

--- heeps/aberrations/__init__.py
+++ heeps/aberrations/__init__.py
@@ -3,5 +3,5 @@ from __future__ import absolute_import
 from .island_effect_piston import island_effect_piston
 from .atmosphere import atmosphere
 from .wavefront_aberrations import wavefront_aberrations
-from .static_ncpa import static_ncpa
+from .ncpa_aberration import ncpa_aberration
 
