7013e1d471b1519809c12a3806f59c9e97ba9c60
Christian <christian.delacroix@gmail.com>
2020-12-03 21:49:20 +0100

    update finite_size tool

--- heeps/util/coord.py
+++ heeps/util/coord.py
@@ -2,16 +2,29 @@ import numpy as np
 
 def polar_coord(npupil):
     
-    xi,yi = cart_coord(npupil)
-    ri = np.abs(xi + 1j*yi)
-    ti = np.angle(xi + 1j*yi)
+    x, y = cart_coord(npupil)
+    r = np.abs(x + 1j*y)
+    t = np.angle(x + 1j*y)
 
-    return ri, ti
+    return r, t
 
 def cart_coord(npupil):
 
     dn = 2/npupil
     pup_range = np.arange(-1, 1, dn) + dn/2
-    xi, yi = np.meshgrid(pup_range, pup_range)
+    x, y = np.meshgrid(pup_range, pup_range)
 
-    return xi, yi
+    return x, y
+
+def disk_coord(radius, nr=4):
+    
+    dr = radius/(nr - 0.5)
+    x, y = [0], [0] # center
+    for r in np.arange(radius/dr)*dr:
+        npts = int(round(2*np.pi*r/dr))
+        for t in np.arange(npts)*2*np.pi/npts:
+            z = r*np.exp(1j*t)
+            x.append(np.real(z))
+            y.append(np.imag(z))
+    
+    return x, y
