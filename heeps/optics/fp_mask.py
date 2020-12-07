eddf28fd78f5cde7fa27fd80ba5f60f12b791d49
Christian <christian.delacroix@gmail.com>
2020-12-07 02:58:36 +0100

    update draw_finite_size for CLC

--- heeps/optics/fp_mask.py
+++ heeps/optics/fp_mask.py
@@ -11,11 +11,10 @@ def fp_mask(wf, mode='RAVC', focal=660, verbose=False, **conf):
     # case 1: vortex coronagraphs
     if mode in ['CVC', 'RAVC']:
         if verbose is True:
-            print('Apply Vortex phase mask')                        
+            print('   apply vortex phase mask')                        
         # update conf
         conf.update(focal=focal)
-        # load vortex calibration files: 
-        #   conf['psf_num'], conf['vvc'], conf['perf_num']
+        # load vortex calibration files: psf_num, vvc, perf_num
         conf = vortex_init(verbose=verbose, **conf)
         # propagate to vortex
         lens(wf, focal)
@@ -25,44 +24,35 @@ def fp_mask(wf, mode='RAVC', focal=660, verbose=False, **conf):
         wf._wfarr = wf._wfarr*conf['vvc'] - wf_corr
         # propagate to lyot stop
         lens(wf, focal)
-
-    # TODO: cleanup CLC
     
     # case 2: classical Lyot
     elif mode in ['CLC']:
         if verbose is True:
-            print('Apply Classical Lyot mask\n')        
-        f_lens = conf['focal']
-        beam_ratio = conf['beam_ratio']
-        CLC_diam = conf['CLC_diam'] # classical lyot diam in lam/D (default to 4)
-        gridsize = conf['gridsize']
-        tmp_dir = conf['temp_dir']
-        
+            print('   apply classical lyot mask')
         # propagate to lyot mask
-        lens(wf, conf['focal'])
-        
+        lens(wf, focal)
         # create or load the classical Lyot mask
-        calib = str(CLC_diam)+str('_')+str(int(beam_ratio*100))+str('_')+str(gridsize)
-        my_file = os.path.join(tmp_dir, 'clc_'+calib+'.fits')
+        calib = str(conf['clc_diam'])+str('_')+str(int(conf['beam_ratio']*100))+str('_')+str(conf['ngrid'])
+        my_file = os.path.join(conf['dir_temp'], 'clc_'+calib+'.fits')
         if not os.path.isfile(my_file):
             # calculate exact size of Lyot mask diameter, in pixels
-            Dmask = CLC_diam/beam_ratio
+            Dmask = conf['clc_diam']/conf['beam_ratio']
             # oversample the Lyot mask (round up)
             samp = 100
             ndisk = int(samp*np.ceil(Dmask))
-            ndisk = ndisk + 1 if not ndisk % 2 else ndisk # must be odd
+            ndisk = ndisk if ndisk % 2 else ndisk + 1# must be odd
             # find center
             cdisk = int((ndisk - 1)/2)
             # calculate the distances to center
             xy = range(-cdisk, cdisk + 1)
             x,y = np.meshgrid(xy, xy)
-            dist = np.sqrt(x**2 + y**2)
+            r = abs(x + 1j*y)
             # create the Lyot mask
             mask = np.zeros((ndisk, ndisk))
-            mask[np.where(dist > samp*Dmask/2)] = 1
+            mask[np.where(r > samp*Dmask/2)] = 1
             # resize to Lyot mask real size, and pad with ones
             mask = resize_img(mask, int(ndisk/samp))
-            mask = pad_img(mask, gridsize, 1)
+            mask = pad_img(mask, conf['ngrid'], 1)
             # write mask
             fits.writeto(my_file, mask)
         else:
@@ -73,6 +63,6 @@ def fp_mask(wf, mode='RAVC', focal=660, verbose=False, **conf):
         wf._wfarr.real *= mask
         
         # propagate to lyot stop
-        lens(wf, conf['focal'])
+        lens(wf, focal)
 
     return wf
