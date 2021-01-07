587cf7fad6aabac72b360a2d0ee50313beed84a7
Christian <christian.delacroix@gmail.com>
2021-01-07 15:58:44 +0100

    added benchmark fft (lens)

--- heeps/optics/lens.py
+++ heeps/optics/lens.py
@@ -1,6 +1,12 @@
 import proper
+import numpy as np
 
-def lens(wfo, f_lens):
-    proper.prop_propagate(wfo, f_lens)
-    proper.prop_lens(wfo, f_lens)
-    proper.prop_propagate(wfo, f_lens)
+def lens(wf, focal):
+    proper.prop_propagate(wf, focal)
+    proper.prop_lens(wf, focal)
+    proper.prop_propagate(wf, focal)
+
+def lens2(wf, focal):
+    proper.prop_propagate(wf, focal)
+    wf._wfarr = np.fft.fft2(wf._wfarr)/wf._ngrid
+    proper.prop_propagate(wf, focal)
