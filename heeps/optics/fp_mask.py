f742a465aea90bce6f2cc4c3be863e4639cdba3d
Christian <christian.delacroix@gmail.com>
2022-02-08 15:54:41 +0100

    light trap update

--- heeps/optics/fp_mask.py
+++ heeps/optics/fp_mask.py
@@ -2,10 +2,9 @@ from .lens import lens
 from .vortex_init import vortex_init
 from .lyotmask_init import lyotmask_init
 import numpy as np
-import proper
 
-def fp_mask(wf, mode='RAVC', vc_zoffset=0, vc_chrom_leak=2e-3, 
-        add_cl_vort=False, verbose=False, **conf):
+def fp_mask(wf, mode='RAVC', vc_zoffset=0, vc_chrom_leak=2e-3, lt_dist=0,
+        lt_diam=0, add_cl_vort=False, verbose=False, **conf):
 
     # case 1: vortex coronagraphs
     if mode in ['CVC', 'RAVC']:
@@ -27,7 +26,8 @@ def fp_mask(wf, mode='RAVC', vc_zoffset=0, vc_chrom_leak=2e-3,
         wf_corr = (conf['psf_num']*conf['vvc'] - conf['perf_num'])*scale_psf
         wf._wfarr = wf._wfarr*conf['vvc'] - wf_corr + wf_cl
         # propagate to lyot stop
-        lens(wf, offset_before=-vc_zoffset, **conf)
+        lens(wf, offset_before=-vc_zoffset, offset_light_trap=lt_dist, 
+            diam_light_trap=lt_diam, **conf)
     
     # case 2: classical Lyot
     elif mode in ['CLC']:
@@ -40,6 +40,6 @@ def fp_mask(wf, mode='RAVC', vc_zoffset=0, vc_chrom_leak=2e-3,
         # apply lyot mask
         wf._wfarr.real *= conf['lyotmask']
         # propagate to lyot stop
-        lens(wf, **conf)
-
+        lens(wf, offset_light_trap=lt_dist, diam_light_trap=lt_diam, **conf)
+    
     return wf
