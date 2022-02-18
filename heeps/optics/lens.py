6ab01dfb939a1e18edc4dac0fe95ccc6b64e6621
Christian <christian.delacroix@gmail.com>
2022-02-18 11:34:44 +0100

    light_trap

--- heeps/optics/lens.py
+++ heeps/optics/lens.py
@@ -3,21 +3,17 @@ import proper
 def lens(wf, focal=660, offset_before=0, offset_after=0, offset_light_trap=0, 
         diam_light_trap=0, **conf):
 
+    # propagation before lens
+    proper.prop_propagate(wf, focal + offset_before)
+    # Fourier transform of an image using a lens
+    proper.prop_lens(wf, focal)
+
     # check for light trap
-    if diam_light_trap == 1:
-        print('no light trap')
-        proper.prop_propagate(wf, offset_light_trap)
-    elif offset_light_trap != 0:
-        print('light trap')
-        proper.prop_propagate(wf, offset_light_trap)
-        proper.prop_circular_aperture(wf, diam_light_trap, 0, 0, NORM=True)
-        proper.prop_propagate(wf, focal - offset_light_trap)
-        proper.prop_lens(wf, focal)
-        proper.prop_propagate(wf, focal)
-    else:
-        # propagation before lens
-        proper.prop_propagate(wf, focal + offset_before)
-        # Fourier transform of an image using a lens
-        proper.prop_lens(wf, focal)
+    if offset_light_trap == 0 and diam_light_trap == 0:
         # propagation after lens
         proper.prop_propagate(wf, focal + offset_after)
+    else:
+        # add light trap
+        proper.prop_propagate(wf, focal - offset_light_trap)
+        proper.prop_circular_aperture(wf, diam_light_trap, 0, 0, NORM=True)
+        proper.prop_propagate(wf, offset_light_trap)
