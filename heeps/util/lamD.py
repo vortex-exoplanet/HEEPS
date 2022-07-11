17b486aa286cf30300c38563a6dd46ba0e351ae0
Christian <christian.delacroix@gmail.com>
2022-07-12 01:16:10 +0200

    bugfix

--- heeps/util/lamD.py
+++ heeps/util/lamD.py
@@ -5,7 +5,7 @@ def get_lamD_pix(lam=3.81e-6, diam_ext=36.905, diam_nominal=38.542,
     
     # 1 lambda/D in mas
     lamD = get_lamD_mas(lam=lam, diam_ext=diam_ext, diam_nominal=diam_nominal, 
-        ls_dRextls_dRext=ls_dRext)
+        ls_dRext=ls_dRext)
     # 1 lambda/D in pixels
     lamD /= pscale
     
