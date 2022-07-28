92129be423aa1f9c990e45112ebf80382d38f168
Christian <christian.delacroix@gmail.com>
2022-07-28 19:06:02 +0200

    update to planet_scene

--- heeps/util/paralang.py
+++ heeps/util/paralang.py
@@ -1,7 +1,17 @@
 import numpy as np
 
 def paralang(npts, dec, lat, duration=3600):
-    """ parallactic angle """
+    """ parallactic angle 
+    
+    Examples:
+        np.sum(abs(paralang(2, -61, -24.59, 3600)))
+        22.870920714712355
+
+        np.sum(abs(paralang(2, -61, -24.59, 3600*5)))
+        104.42140816101042
+        
+    """
+
     # hour angle to deg conversion
     ha = duration/3600*360/24
     # angles in rad
