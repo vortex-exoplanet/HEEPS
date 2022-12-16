15269fd18f85d331f1d068dec5237f13d26e3e98
Christian <christian.delacroix@gmail.com>
2022-12-16 14:36:37 +0100

    debugging light_trap_IMG notebook

--- heeps/optics/fp_mask.py
+++ heeps/optics/fp_mask.py
@@ -14,7 +14,7 @@ def fp_mask(wf, mode='RAVC', vc_zoffset=0, vc_chrom_leak=2e-3, lt_dist_start=0,
         # load vortex calibration files: psf_num, vvc, perf_num
         conf = vortex_init(verbose=verbose, **conf)
         # propagate to vortex
-        lens(wf, vc_zoffset_before=vc_zoffset, **conf)
+        lens(wf, zoffset_before=vc_zoffset, **conf)
         # load chromatic leakage
         if add_cl_vort is True:
             if verbose is True:
@@ -27,9 +27,9 @@ def fp_mask(wf, mode='RAVC', vc_zoffset=0, vc_chrom_leak=2e-3, lt_dist_start=0,
         wf_corr = (conf['psf_num']*conf['vvc'] - conf['perf_num'])*scale_psf
         wf._wfarr = wf._wfarr*conf['vvc'] - wf_corr + wf_cl
         # propagate to lyot stop (lt = light trap)
-        lens(wf, vc_zoffset_after=-vc_zoffset, 
-            lt_dist_start=lt_dist_start, lt_diam_start=lt_diam_start,
-            lt_dist_end=lt_dist_end, lt_diam_end=lt_diam_end, **conf)
+        lens(wf, zoffset_after=-vc_zoffset, 
+            dist_start=lt_dist_start, diam_start=lt_diam_start,
+            dist_end=lt_dist_end, diam_end=lt_diam_end, **conf)
     
     # case 2: classical Lyot
     elif mode in ['CLC']:
@@ -42,6 +42,7 @@ def fp_mask(wf, mode='RAVC', vc_zoffset=0, vc_chrom_leak=2e-3, lt_dist_start=0,
         # apply lyot mask
         wf._wfarr.real *= conf['lyotmask']
         # propagate to lyot stop
-        lens(wf, offset_light_trap=lt_dist, diam_light_trap=lt_diam, **conf)
+        lens(wf, dist_start=lt_dist_start, diam_start=lt_diam_start,
+            dist_end=lt_dist_end, diam_end=lt_diam_end, **conf)
     
     return wf
--- heeps/optics/lens.py
+++ heeps/optics/lens.py
@@ -1,33 +1,39 @@
 import proper
+from astropy.io import fits
 
-def lens(wf, focal=660, vc_zoffset_before=0, vc_zoffset_after=0,
-        lt_dist_start=0, lt_diam_start=0, lt_dist_end=0, lt_diam_end=0, **conf):
+def lens(wf, focal=660, zoffset_before=0, zoffset_after=0,
+        dist_start=0, diam_start=0, dist_end=0, diam_end=0,
+        lt_write_name=None, **conf):
 
     # check for light trap BEFFORE lens
-    if lt_dist_start == 0 and lt_diam_start == 0:
+    if dist_start == 0 and diam_start == 0:
         # propagation before lens
-        proper.prop_propagate(wf, focal + vc_zoffset_after)
+        proper.prop_propagate(wf, focal + zoffset_after)
     else:
         # add light trap
-        proper.prop_propagate(wf, lt_dist_start)
-        if type(lt_diam_start) is tuple:
-            proper.prop_rectangular_aperture(wf, lt_diam_start[0], lt_diam_start[1], 0, 0, NORM=True)
+        proper.prop_propagate(wf, dist_start)
+        if type(diam_start) is tuple:
+            proper.prop_rectangular_aperture(wf, diam_start[0], diam_start[1], 0, 0, NORM=True)
         else:
-            proper.prop_circular_aperture(wf, lt_diam_start, 0, 0, NORM=True)
-        proper.prop_propagate(wf, focal - lt_dist_start + vc_zoffset_after)
+            proper.prop_circular_aperture(wf, diam_start, 0, 0, NORM=True)
+        if lt_write_name is not None:
+            fits.writeto(lt_write_name, proper.prop_get_amplitude(wf), overwrite=True)
+        proper.prop_propagate(wf, focal - dist_start + zoffset_after)
     
     # Fourier transform of an image using a lens
     proper.prop_lens(wf, focal)
 
     # check for light trap AFTER lens
-    if lt_dist_end == 0 and lt_diam_end == 0:
+    if dist_end == 0 and diam_end == 0:
         # propagation after lens
-        proper.prop_propagate(wf, focal + vc_zoffset_before)
+        proper.prop_propagate(wf, focal + zoffset_before)
     else:
         # add light trap
-        proper.prop_propagate(wf, focal - lt_dist_end)
-        if type(lt_diam_end) is tuple:
-            proper.prop_rectangular_aperture(wf, lt_diam_end[0], lt_diam_end[1], 0, 0, NORM=True)
+        proper.prop_propagate(wf, focal - dist_end)
+        if type(diam_end) is tuple:
+            proper.prop_rectangular_aperture(wf, diam_end[0], diam_end[1], 0, 0, NORM=True)
         else:
-            proper.prop_circular_aperture(wf, lt_diam_end, 0, 0, NORM=True)
-        proper.prop_propagate(wf, lt_dist_end + vc_zoffset_before)
+            proper.prop_circular_aperture(wf, diam_end, 0, 0, NORM=True)
+        if lt_write_name is not None:
+            fits.writeto(lt_write_name, proper.prop_get_amplitude(wf), overwrite=True)
+        proper.prop_propagate(wf, dist_end + zoffset_before)
