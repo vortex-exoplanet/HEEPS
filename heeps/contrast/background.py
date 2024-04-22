4ca5b72f4bc2a76e7846f006a8fe947bcbf4e591
Christian <christian.delacroix@gmail.com>
2024-04-22 13:06:26 +0200

    update to star-hopping cubes

--- heeps/contrast/background.py
+++ heeps/contrast/background.py
@@ -80,7 +80,7 @@ def background(psf_ON, psf_OFF, header=None, mode='RAVC', lam=3.8e-6, dit=0.3,
         psf_ON += np.random.normal(0, np.sqrt(psf_ON))
 
     if verbose is True:
-        print('   mag=%s, thruput=%.4f, mask_trans=%.4f,'%(mag, thruput, mask_trans))
-        print('   star_signal=%.2e, bckg_noise=%.2e'%(star_signal, bckg_noise))
+        print('   dit=%sms, thruput=%.4f, mask_trans=%.4f,'%(dit, thruput, mask_trans))
+        print('   mag=%s, star_signal=%.2e, bckg_noise=%.2e'%(mag, star_signal, bckg_noise))
 
     return psf_ON, psf_OFF
