87467d4f14bd32f4a9f69d3ab6fff91944347938
Christian <christian.delacroix@gmail.com>
2024-11-26 13:55:22 +0100

    bugfix in hexagonal segments centring

--- heeps/pupil/create_hexagon.py
+++ heeps/pupil/create_hexagon.py
@@ -7,7 +7,7 @@ def create_hexagon(ngrid, hexrad, xc, yc, sampling, mag=11):
     Input
     ----------    
     ngrid : int
-        grid size
+        grid size (must be even)
         
     hexrad : float
         Distance from any vertex to the center of the hexagon
@@ -30,12 +30,12 @@ def create_hexagon(ngrid, hexrad, xc, yc, sampling, mag=11):
     ngrid *= 2
     
     image = np.zeros([ngrid, ngrid], dtype = np.float64)
-    xcpix = xc / sampling + ngrid/2 - 1
-    ycpix = yc / sampling + ngrid/2 - 1
+    xcpix = xc / sampling + (ngrid - 1)/2
+    ycpix = yc / sampling + (ngrid - 1)/2
     radpix = hexrad/sampling
     
     # angles for verices of hexagon
-    angles = np.array([90., 30., -30., -90., 210, 150])
+    angles = np.array([90, 30, -30, -90, 210, 150])
     
     xv = np.cos(np.deg2rad(angles))
     yv = np.sin(np.deg2rad(angles))
@@ -44,20 +44,16 @@ def create_hexagon(ngrid, hexrad, xc, yc, sampling, mag=11):
     yp = yv * radpix + ycpix
         
     yindex = np.round([yp[3], yp[4], yp[5], yp[0]+1]).astype(int)
-    
     slopes = 1 / np.tan(np.deg2rad([30, 90, 150]))
     
     for l in range(len(yindex)-1):
         for ypix in range(yindex[l], yindex[l+1]):
-            mag2 = mag
             for ysub in range(0, mag):
                 y = ypix - 0.5 + (0.5 + ysub)/mag # this breaks a pixel into mag steps
-                
                 if y < yp[3]:
                     continue # if true skips rest of the code, to exclude values y<yp[left]
                 if y > np.max(yp[0]):
                     break # this is to break the magnification loop when y reaches max(yp)
-                   
                 if y<yp[4]:
                     ind = 0
                 if y < yp[5] and y >= yp[4]:
@@ -68,23 +64,20 @@ def create_hexagon(ngrid, hexrad, xc, yc, sampling, mag=11):
                 xright = slopes[ind] * (y-yp[3-ind]) + xp[3-ind]
                 xleftpix = int(np.round(xleft))
                 xrightpix = int(np.round(xright))
-                
                 if xleftpix != xrightpix:
                     image[ypix,xleftpix] = image[ypix,xleftpix] + mag * ((xleftpix + 0.5) - xleft)
                     image[ypix,xrightpix] = image[ypix,xrightpix] + mag * (xright - (xrightpix - 0.5))
-                    
                     if (xrightpix - xleftpix) > 1:
-                        imin = xleftpix+1
+                        imin = xleftpix + 1
                         imax = xrightpix
                         image[ypix,imin:imax] +=  mag
                 else:
                     image[ypix,xleftpix] = image[ypix,xleftpix] + mag * (xright - xleft)
     
-    image = image / float(mag)**2
+    image /= float(mag)**2
     
     # crop final image
     crop = int(ngrid/4)
     image = image[crop:3*crop, crop:3*crop]
         
     return image
-
