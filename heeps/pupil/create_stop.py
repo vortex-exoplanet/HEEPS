6247cac6f72133eb1434454650c3a4a64c6e5e0d
GillesOrban <gorban@uliege.be>
2025-03-13 17:53:11 +0100

    Updating ELT pupil and LS stops notebook

--- heeps/pupil/create_stop.py
+++ heeps/pupil/create_stop.py
@@ -8,7 +8,7 @@ import numpy as np
 def create_stop(ngrid=2**10, npupil=285, pupil_img_size=40, diam_nominal=38,
         diam_ext=37, diam_int=11, spi_angles=[0,60,120,180,240,300],
         spi_width=0.54, AP_angles=[], AP_width=0, AP_center=1, AP_length=2,
-        dRext=0, dRint=0, dRspi=0, dx=0, dy=0,
+        dRext=0, dRint=0, dRspi=0, force_sym=False, dx=0, dy=0,
         circ_ext=True, circ_int=True, add_seg=False,
         seg_ny=[10,13,16,19,22,23,24,25,26,27,28,29,30,31,30,31,
         30,31,30,31,30,31,30,29,28,27,26,25,24,23,22,19,16,13,10],
@@ -44,6 +44,10 @@ def create_stop(ngrid=2**10, npupil=285, pupil_img_size=40, diam_nominal=38,
             asymetric pupil spider normalized length wrt nominal radius
         dRext, dRint, dRspi: floats
             margins for diam_ext, diam_int, spi_width, wrt diam_nominal
+        force_sym : bool
+            if True and dRspi is not 0, force the spiders to have all the same dimension,
+            max(spi_width, AP_width). Specially useful to force symmetric Lyot stop.
+            Default: False
         dx, dy: floats
             percentage of misalignment in x and y
         circ_ext, circ_int: bools
@@ -109,22 +113,37 @@ def create_stop(ngrid=2**10, npupil=285, pupil_img_size=40, diam_nominal=38,
             mask_int = np.hstack([np.zeros((ngrid, 1)), np.vstack([np.zeros((1, ngrid-1)), mask_int])])
             proper.prop_multiply(wf_tmp, mask_int)
 
-    # regular spiders
-    if len(spi_angles) > 0:
-        for angle_deg in spi_angles:
-            angle_rad = np.radians(angle_deg)
-            spi_length = pupil_img_size/diam_nominal
-            proper.prop_rectangular_obscuration(wf_tmp, 2 * (spi_width/diam_nominal + dRspi), spi_length,
-                np.sin(angle_rad)*spi_length/2 + dx, -np.cos(angle_rad)*spi_length/2 + dy,
-                ROTATION=angle_deg, NORM=True) # NORM: nominal radius = 1
-    
-    # asymetric pupil spiders
-    if len(AP_angles) > 0:
-        for angle_deg in AP_angles:
-            angle_rad = np.radians(angle_deg)
-            proper.prop_rectangular_obscuration(wf_tmp, 2 * (AP_width/diam_nominal + dRspi), AP_length,
-                np.sin(angle_rad)*AP_center + dx, -np.cos(angle_rad)*AP_center + dy,
-                ROTATION=angle_deg, NORM=True) # NORM: nominal radius = 1
+    if (dRspi != 0) and force_sym:
+        # Lyot stop oversize handling in case we want to force the same spiders sizes.
+        print('[Warning] Forcing spiders to have all the same sizes.')
+        new_spi_width = np.max([spi_width, AP_width])
+        new_spi_angles = set(spi_angles)     # Convert the original list to a set to remove duplicates
+        new_spi_angles.update(AP_angles)    # Add elements from the new list to the set
+        new_spi_angles = list(new_spi_angles)    # Convert the set back to a list
+        if len(new_spi_angles) > 0:
+            for angle_deg in new_spi_angles:
+                angle_rad = np.radians(angle_deg)
+                spi_length = pupil_img_size/diam_nominal
+                proper.prop_rectangular_obscuration(wf_tmp, 2 * (new_spi_width/diam_nominal + dRspi), spi_length,
+                    np.sin(angle_rad)*spi_length/2 + dx, -np.cos(angle_rad)*spi_length/2 + dy,
+                    ROTATION=angle_deg, NORM=True) # NORM: nominal radius = 1
+    else:
+        # regular spiders
+        if len(spi_angles) > 0:
+            for angle_deg in spi_angles:
+                angle_rad = np.radians(angle_deg)
+                spi_length = pupil_img_size/diam_nominal
+                proper.prop_rectangular_obscuration(wf_tmp, 2 * (spi_width/diam_nominal + dRspi), spi_length,
+                    np.sin(angle_rad)*spi_length/2 + dx, -np.cos(angle_rad)*spi_length/2 + dy,
+                    ROTATION=angle_deg, NORM=True) # NORM: nominal radius = 1
+        
+        # asymetric pupil spiders
+        if len(AP_angles) > 0:
+            for angle_deg in AP_angles:
+                angle_rad = np.radians(angle_deg)
+                proper.prop_rectangular_obscuration(wf_tmp, 2 * (AP_width/diam_nominal + dRspi), AP_length,
+                    np.sin(angle_rad)*AP_center + dx, -np.cos(angle_rad)*AP_center + dy,
+                    ROTATION=angle_deg, NORM=True) # NORM: nominal radius = 1
 
     # crop the pupil to pupil_img_size => (ngrid-1)
     pup = proper.prop_get_amplitude(wf_tmp)[1:,1:]
