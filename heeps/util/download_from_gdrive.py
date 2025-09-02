34283f53f0b29aa8604d982ea649a36c2079452e
Gilles Orban de Xivry <gorban@ulg.ac.be>
2025-09-02 17:19:56 +0200

    gdrive download docstring and improving verbose

--- heeps/util/download_from_gdrive.py
+++ heeps/util/download_from_gdrive.py
@@ -17,7 +17,30 @@ def extract_zip(googleID, destination):
     os.remove(zipfilename)
 
 def download(googleID, destination, filename, 
-        url='https://docs.google.com/uc?export=download'):
+        url='https://docs.google.com/uc?export=download', 
+        verbose=False):
+    """
+    Downloads a file from Google Drive given its ID, saving it to the
+    specified destination with the given filename.
+
+    Parameters
+    ----------
+    googleID : str
+        The ID of the file on Google Drive.
+    destination : str
+        The directory where the file should be saved.
+    filename : str
+        The filename for the saved file.
+    url : str
+        The base URL for the download. Defaults to
+        'https://docs.google.com/uc?export=download'.
+
+    Notes
+    -----
+    Will only download the file if it does not already exist in the
+    destination directory. If the file is larger than 25MB, a
+    confirmation token must be extracted from the initial response.
+    """
     # path to file
     my_file = str(os.path.join(destination, filename))
     if not os.path.isfile(my_file): 
@@ -25,6 +48,9 @@ def download(googleID, destination, filename,
         assert check_internet(), \
                 "HEEPS can't download input files. Check your internet connection."
         response = session.get(url, params={'id':googleID}, stream=True)
+        if verbose:
+            print('Response : ', response.text)
+            print('Name of the file: ', extract_filename_from_response(response.text))
         token, url = extract_confirmation_token(response.text)
         if token:
             response = session.get(url, params={'id':googleID, \
@@ -63,6 +89,22 @@ def extract_confirmation_token(html_content):
         download_url = "https://docs.google.com/uc?export=download"
 
     return token, download_url
+
+def extract_filename_from_response(html_content):
+    soup = BeautifulSoup(html_content, 'html.parser')
+    
+    # Find the <span class="uc-name-size"> tag
+    name_span = soup.find('span', class_='uc-name-size')
+    if name_span:
+        # Inside this span, there's an <a> tag with the file name
+        file_link = name_span.find('a')
+        if file_link:
+            file_name = file_link.text.strip()
+            print(f"Extracted file name: {file_name}")
+            return file_name
+
+    print("File name not found in response.")
+    return None
 
 def save_response_content(response, my_file, chunk_size=32768):
     with open(my_file, "wb") as f:
